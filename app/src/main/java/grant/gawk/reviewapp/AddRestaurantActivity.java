c886626cce858db9e12d3240c293aeb1357e37d1
alexanderg931 <grant931@gmail.com>
2017-03-11 22:56:38 -0600

    3/11/2017 - Fully documented File Handler! Also fixed an error in file handler where a else in writeDish should have been causing our first dish object on a new file to not be added to the file. I'm not sure if it actually was causing that error, but it's not possible now because I removed the else. Also added Add Picture button to AddRestaurantActivity. It still doesn't work, and the button is still hardcoded in File Handler to pass a null image, so for demonstrative purposes, the program still works without image capabilities.

--- app/src/main/java/grant/gawk/reviewapp/AddRestaurantActivity.java
+++ app/src/main/java/grant/gawk/reviewapp/AddRestaurantActivity.java
@@ -23,6 +23,7 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnClickL
     Context appContext;
     ImageView getImage;
     Button btnSubmit;
+    Button takePicture;
     EditText restName, cityName;
 
 
@@ -32,7 +33,6 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnClickL
         setContentView(R.layout.activity_add_restaurant);
         setTitle("Add a Restaurant");
         appContext = this.getApplicationContext();
-        lvRestaurant = (ListView)findViewById(R.id.listRestaurant);
         cityName = (EditText) findViewById(R.id.cityName);
         restName = (EditText) findViewById(R.id.restName);
 
@@ -40,12 +40,16 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnClickL
         List<Restaurant> listRest = new ArrayList<Restaurant>();
     }
 
-
+    public void takePicture(View v)
+    {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI); //Allow the gallery to be opened
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+    }
     @Override
     public void onClick(View v)
     {
         switch (v.getId()) {
-            case R.id.getImage:
+            case R.id.takePicture:
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI); //Allow the gallery to be opened
                 startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
                 break;
@@ -67,9 +71,10 @@ public class AddRestaurantActivity extends AppCompatActivity implements OnClickL
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) //make sure to receive result from gallery
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data.getData() != null) //make sure to receive result from gallery
         {
             Uri selectedImage = data.getData(); //Get the uri, allows to prefer back to the image
+            System.out.println(selectedImage.getPath());
             getImage.setImageURI(selectedImage); //display the image
         }
     }
