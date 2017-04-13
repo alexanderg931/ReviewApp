dec70bb7f7cf82cc998f6c569823351feed7c48c
AnthonyP4312 <anthonyp4312@gmail.com>
2017-04-12 21:48:01 -0500

    Added Restaurant sorting by name

--- app/src/main/java/grant/gawk/reviewapp/SettingsFragment.java
+++ app/src/main/java/grant/gawk/reviewapp/SettingsFragment.java
@@ -4,6 +4,7 @@ package grant.gawk.reviewapp;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import static android.content.ContentValues.TAG;
@@ -40,11 +41,11 @@ public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        //Makes settings menu not have a transparent background.
+//        Makes settings menu not have a transparent background.
 //        getView().setBackgroundColor(ContextCompat.getColor(
-//                getActivity().getApplicationContext(), R.color.colorAccent));
+//                getActivity().getApplicationContext(), R.color.primary_material_light_1));
         try{
-            getView().setBackgroundColor(Color.DKGRAY);}
+            getView().setBackgroundColor(Color.WHITE);}
         catch(NullPointerException e){
             Log.d(TAG, "onActivityCreated: NullPointerThrown");
         }
